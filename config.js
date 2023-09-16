a28c491fdbc93c2b1b6c308f3a018031fde32f99
Konrad Borowski <konrad@borowski.pw>
2023-09-16 13:02:28 +0200

    Configure Renovate

---
+++ config.js
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2023 Konrad Borowski <konrad@borowski.pw>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+module.exports = {
+    gitAuthor: "xfix's Renovate Bot <renovate@borowski.pw>",
+    repositories: ['xfix/xfix.pw'],
+    onboardingConfig: {
+        $schema: "https://docs.renovatebot.com/renovate-schema.json",
+        extends: ["config:recommended"],
+    },
+};
